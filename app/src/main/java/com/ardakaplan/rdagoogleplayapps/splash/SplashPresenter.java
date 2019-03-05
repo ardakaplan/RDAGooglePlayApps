570a0ae73a13715b0bfa9cf7a3739837316aa5ae
Arda Kaplan <arda.kaplan@matriksdata.com>
2019-03-05 16:09:39 +0300

    temizlik

--- app/src/main/java/com/ardakaplan/rdagoogleplayapps/splash/SplashPresenter.java
+++ app/src/main/java/com/ardakaplan/rdagoogleplayapps/splash/SplashPresenter.java
@@ -1,23 +1,16 @@
 package com.ardakaplan.rdagoogleplayapps.splash;
 
-import com.ardakaplan.rdagoogleplayapps.domain.GetListRDAInteraction;
-import com.ardakaplan.rdalibrary.base.interactions.RDAInteractionResult;
-import com.ardakaplan.rdalibrary.base.interactions.RDAInteractionResultListener;
 import com.ardakaplan.rdalibrary.base.ui.screen.RDAPresenter;
-import com.ardakaplan.rdalogger.RDALogger;
-
-import java.util.ArrayList;
 
 import javax.inject.Inject;
 
 public class SplashPresenter extends RDAPresenter<SplashContract.SplashViewContract> implements SplashContract.SplashPresenterContract {
 
-    private GetListRDAInteraction getListInteraction;
 
     @Inject
-    public SplashPresenter(GetListRDAInteraction getListInteraction) {
+    public SplashPresenter() {
 
-        this.getListInteraction = getListInteraction;
+
     }
 
     @Override
@@ -30,34 +23,5 @@ public class SplashPresenter extends RDAPresenter<SplashContract.SplashViewContr
 
     }
 
-    @Override
-    public void testPresenterContract() {
 
-        RDALogger.info("SPLASH PRESENTER ÇALIŞTI");
-    }
-
-    @Override
-    public void getList() {
-
-        getListInteraction.setIn("LAHANA");
-
-        getListInteraction.execute(new RDAInteractionResultListener<ArrayList<String>>() {
-
-            @Override
-            public void onResult(RDAInteractionResult<ArrayList<String>> out) {
-
-                if (gettView() != null) {
-
-                    if (out.isSuccess()) {
-
-                        gettView().setList(out.getOut());
-
-                    } else {
-
-                        gettView().onError(out.getException());
-                    }
-                }
-            }
-        });
-    }
 }
