8328c8f39db041b4a2294704a1544f17709b9921
Arda Kaplan <arda.kaplan@matriksdata.com>
2019-05-20 10:22:07 +0300

    retrofit 1.0.3

--- app/src/main/java/com/ardakaplan/rdagoogleplayapps/TestApplication.java
+++ app/src/main/java/com/ardakaplan/rdagoogleplayapps/TestApplication.java
@@ -5,39 +5,23 @@ import com.ardakaplan.rdagoogleplayapps.di.DaggerAppComponent;
 import com.ardakaplan.rdalibrary.base.objects.RDAApplication;
 import com.ardakaplan.rdalibrary.di.HasCustomActivityInjector;
 import com.ardakaplan.rdalogger.RDALogger;
-import com.ardakaplan.rdaretrofitlib.retrofit.RDARetrofitProvider;
-
-import java.util.ArrayList;
-import java.util.List;
-
-import okhttp3.logging.HttpLoggingInterceptor;
 
 /**
  * Created by Arda Kaplan on 10.08.2018 - 17:44
  */
 public class TestApplication extends RDAApplication implements HasCustomActivityInjector {
 
-    private static AppComponent appComponent;
-
     @Override
     public void onCreate() {
         super.onCreate();
 
         RDALogger.start(getString(R.string.app_name)).enableLogging(true);
-
-        initRDARetrofitLib();
-    }
-
-    private void initRDARetrofitLib() {
-        RDARetrofitProvider.RetrofitManager.TIME_OUT = 20;
-        RDARetrofitProvider.RetrofitManager.LOGGING_LEVEL = HttpLoggingInterceptor.Level.BODY;
-
     }
 
     @Override
     protected void initDagger() {
 
-        appComponent = DaggerAppComponent.builder().application(this).build();
+        AppComponent  appComponent = DaggerAppComponent.builder().application(this).build();
 
         appComponent.inject(this);
     }
